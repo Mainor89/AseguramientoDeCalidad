3cc07763fd63722cd1faf7c17b097c35811a9d1d
fernanmatoro <fernanmatoro@gmail.com>
2015-03-14 05:43:03 -0600

    Función del año bisiesto que retorna booleano
    Función modificada para que retorne un valor booleano correspondiente al
    evaluar si el año es bisiesto

--- determinar_bisiesto.js
+++ determinar_bisiesto.js
@@ -1,11 +1,10 @@
 
 /* La función comprobar_bisiesto determina si el año que se recibe
  por parametro es un año bisiesto, es decir si posee 366 días. El parametro que recibe
-la función es un número entero positivo  */
+la función es un número entero positivo, el valor que retorna es un booleano, verdadero en caso 
+que cumpla las condiciones establecidas y falso en caso contrario */
 
 function comprobar_bisiesto(año){
-
-var a = parseInt(año);
 
 /*Instruccion if que verifica las condiciones para que
 un año sea bisiesto, el año bisisesto debe ser divisible
@@ -14,18 +13,20 @@ por ejemplo: los años divisibles por 4 son bisiestos siempre y cuando no sean di
 en caso que el año cumpla las 2 condiciones anteriores y además es divisible por 400, ese año
 será bisiesto.
 */
-if((a%4==0)&((a%100 !=0)|(a%400==0))) { 
+
+if((año%4==0)&((año%100 !=0)|(año%400==0))) { 
 
 
-alert("es bisiesto"); //muestra un mensaje indicando que el año es bisiesto
+return true; //retorna true en caso el año es bisiesto
    
 } 
 
 else { 
 
 /*En caso que el año no cumpla las condiciones necesarias
-se muestra un mensaje indicando que el año no es bisiesto*/
-alert("no es bisiesto"); 
+retorna false*/
+
+return false; 
   
 }
 
