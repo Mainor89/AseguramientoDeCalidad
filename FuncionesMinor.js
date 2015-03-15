776091cadc560a9775d61568c5d952230ab2c884
Minor Pizarro <mpizarro@intelligentsense.com>
2015-03-15 14:16:07 -0600

    Funciones para R0 y R4
    Aún tengo q validar lo de Febrero para ver si tiene 28 o 29 días pero ya
    casi hago eso, por lo demas ya esta funcionando ambos casos, estoy
    viendo interfaz ahora jeje porq eso si no se

---
+++ FuncionesMinor.js
@@ -0,0 +1,83 @@
+//Lista de dias para determinar segun la formula el día que será el primero de Enero
+var DaysOfTheWeek = [
+	{id:"0", name:"Domingo"},
+	{id:"1", name:"Lunes"},
+	{id:"2", name:"Martes"},
+	{id:"3", name:"Miércoles"},
+	{id:"4", name:"Jueves"},
+	{id:"5", name:"Viernes"},
+	{id:"6", name:"Sábado"}
+	];
+
+//Lista de los meses con los dias que tienen, febrero no esta contemplado como bisiesto
+var MonthDays = [
+	{id:"0", days:31},
+	{id:"1", days:28},
+	{id:"2", days:31},
+	{id:"3", days:30},
+	{id:"4", days:31},
+	{id:"5", days:30},
+	{id:"6", days:31},
+	{id:"7", days:31},
+	{id:"8", days:30},
+	{id:"9", days:31},
+	{id:"10", days:30},
+	{id:"11", days:31}
+];
+
+function Prueba(){
+	alert(CalcularPrimerDia(2015));
+	var monthDates = CalcularMes(3,2015);
+	for(var index = 0, size = monthDates.length; index < size; index++){
+		console.log(monthDates[index].day + " " + monthDates[index].name);
+	}	
+}
+
+//Dado un mes y un año retorna la fecha de inicio de ese mes
+function CalcularPrimerDiaMes(pMonth, pYear){
+	//Calcula el mes, orden de meses Marzo=1, Abril=2, .., Enero =11, Febrero =12
+	var month = ((pMonth + 12) - 2)%12;
+	if(month === 0){
+		month = 12;
+	}	
+	//valor del día que queremos buscar
+	var day = 1;
+	//primeros dos dígitos del año
+	var century = Math.floor(pYear / 100);
+	//Antes los meses iban de marzo a Febrero, por lo q si se busca el 1 de Enero del 2015
+	//se debera buscar por el 1 de enero del 2014 por eso la sustracción siguiente
+	var year = pYear % 100;
+	if(month === 11 || month === 12){
+		year = (pYear - 1) % 100;
+	}
+	//Calculos de la formula para simplificarla
+	var firstCalculation = Math.floor((2.6 * month) - 0.2);
+	var secondCalculation = Math.floor(year / 4);
+	var thirdCalculation = Math.floor(century / 4);
+	//El resultado será un número del 0 al 6, ese valor indicará el día de la semana
+	var result = (day + parseInt(firstCalculation) - (2 * century) + year + parseInt(secondCalculation) + parseInt(thirdCalculation)) % 7;
+	if(result < 0){
+		result = result + 7;
+	}
+	return result;
+}
+
+
+//Método que permitira calcular el dia que sera el 1 de Enero para el año dado
+function CalcularPrimerDia(pYear){	
+	var result = CalcularPrimerDiaMes(1,pYear);	
+	return DaysOfTheWeek[result].name;	
+}
+
+//Funcion que dado el mes y año creará todas las fechas de dicho mes
+function CalcularMes(pMonth, pYear){
+	var monthResult = [];
+	var monthStartDay = CalcularPrimerDiaMes(pMonth, pYear);
+	var monthId = pMonth - 1;
+	monthResult.push({day:1, name:DaysOfTheWeek[monthStartDay].name});
+	for (var day = 2, monthSize = MonthDays[monthId].days; day <= monthSize; day++) {
+		monthStartDay++;
+		monthResult.push({day:day, name:DaysOfTheWeek[monthStartDay%7].name});
+	};
+	return monthResult;
+}
